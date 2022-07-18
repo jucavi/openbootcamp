607a4103d00f4613613fcc8229744242ab2c9136
Jucavi <jcarvil@gmail.com>
2022-07-18 21:44:26 +0200

    ejercicio1 tema 4

---
+++ python/ejercicio1_tema4.py
@@ -0,0 +1,6 @@
+edad = int(input('Por favor ingrese su edad: '))
+
+if edad >= 18:
+    print('Eres mayor de edad.')
+else:
+    print('Aun eres menor de edad.')
---
+++ python/ejercicio2_tema3.py
@@ -0,0 +1,4 @@
+peso = float(input('Por favor ingrese su peso en kg: '))
+altura = float(input('Por favor ingrese su altura en metros: '))
+
+print(f'Tu índice de masa corporal es: {peso / altura ** 2:.2f} kg/m^2')
