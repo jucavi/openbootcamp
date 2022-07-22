593809c1107e6095700b6efc62d675562d79356f
Jucavi <jcarvil@gmail.com>
2022-07-22 21:54:40 +0200

    ejercicio 2 tema 7 con minutos

--- python/ejercicio2_tema7.py
+++ python/ejercicio2_tema7.py
@@ -1,9 +1,9 @@
 import time
 
 tiempo_ahora = time.localtime()
-hora_de_salida = 19
+hora_de_salida = 22
 
 if tiempo_ahora.tm_hour >= hora_de_salida:
     print('Party time!!!!')
 else:
-    print(f'{hora_de_salida - tiempo_ahora.tm_hour} horas aun para ir a casa')
+    print(f'{hora_de_salida - tiempo_ahora.tm_hour} horas  y  {59 - tiempo_ahora.tm_min} aun para ir a casa')
