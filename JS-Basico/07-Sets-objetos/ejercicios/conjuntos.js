adddf804b446b6c3c25d8119c96bd04dfb593be7
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-13 11:51:48 +0200

    ejercicio conjuntos

---
+++ JS-Basico/07-Sets-objetos/ejercicios/conjuntos.js
@@ -0,0 +1,17 @@
+const nombresFamiliares = new Set([
+  'Ana',
+  'Juan',
+  'Carlos',
+  'Omar',
+  'Candida',
+  'Maira'
+]);
+
+nombresFamiliares.add('Juan Carlos');
+nombresFamiliares.add('Juan Carlos');
+
+console.log(nombresFamiliares);
+
+nombresFamiliares.add('JavaScript');
+
+console.log(nombresFamiliares);
