fa7d15620b31f1a63339e41f4f777d2a4ede5a0f
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-15 13:42:39 +0200

    ejercicio 11

---
+++ JS-Basico/Ejercicios/11-clases/estudiante.js
@@ -0,0 +1,19 @@
+class Estudiante {
+    constructor(nombre, asignaturas = ['JavaScript', 'HTML', 'CSS']) {
+        this.nombre = nombre;
+        this.asignaturas = asignaturas;
+    }
+
+    obtenDatos() {
+        return {
+            nombre: this.nombre,
+            asignaturas: this.asignaturas
+        }
+    }
+}
+
+const estudiante = new Estudiante('Juan Carlos');
+console.log(estudiante.obtenDatos());
+
+const nuevo_estudiante = new Estudiante('Felix', ['Óptica', 'Análisis Mátematico I', 'Física Cuantica']);
+console.log(nuevo_estudiante.obtenDatos());
