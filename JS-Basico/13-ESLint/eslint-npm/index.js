ecef2f76575fdcd8d977a002cdb14439406b31fc
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-23 22:34:04 +0200

    eslint

---
+++ JS-Basico/13-ESLint/eslint-npm/index.js
@@ -0,0 +1,16 @@
+// Este es el archivo que utilizaremos como ejemplo
+const dog = "New dog inside"
+
+// eslint-disable-next-line quotes
+const persona = 'Maria'
+
+// eslint-disable-next-line indent
+    console.log(dog)
+
+console.log()
+
+// fixed with npm run lint-fix scrip in package.json
+const felix = "nombre de felix"
+
+
+
