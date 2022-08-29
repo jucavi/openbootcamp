eb2a94af9d64bb6dfffec54a19e519ef002c84fb
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-29 13:36:36 +0200

    agregado el ejercicio 1

---
+++ Java-Basico/Ejercicios/ejercicio_1/src/com/openbootcamp/Solution1.java
@@ -0,0 +1,36 @@
+package com.openbootcamp;
+
+/**
+ * Clase requerida en la solución del ejercicio propuesto
+ */
+public class Solution1 {
+
+    /**
+     * Punto de entrada de la aplicación java
+     * Permite ejecutar la aplicación
+     * @param args - argumentos de entrada de la aplicación
+     */
+    public static void main(String[] args) {
+
+        // tipos numéricos
+        // enteros
+        byte var1 = 2;      // 1 byte
+        short var2 = 12;    // 2 byte
+        int var3 = 45;      // 4 byte
+        long var4 = 1000;   // 8 byte
+
+        // punto flotante
+        float var5 = 1.0f;  // por defecto la signación es como double, por eso la f final
+        double var6 = 4.8;
+
+
+        // booleanos
+        boolean var7 = true;
+        boolean var8 = false;
+
+        // texto
+        char var9 = 'a'; // notese el uso de comillas simples
+        String var10 = "Esta es una cadena de caracteres de muestra";
+    }
+
+}
