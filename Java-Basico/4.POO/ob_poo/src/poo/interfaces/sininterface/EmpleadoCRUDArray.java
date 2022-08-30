ce2fd6d5b28011b78f5b0a09ebecf0618bdf9fa4
jucavi <jcarvil@gmail.com>
2022-08-30 10:10:45 +0200

    sinempleados empleados crud array

--- Java-Basico/4.POO/ob_poo/src/poo/interfaces/sininterface/EmpleadoCRUDArray.java
+++ Java-Basico/4.POO/ob_poo/src/poo/interfaces/sininterface/EmpleadoCRUDArray.java
@@ -1,6 +1,25 @@
 package poo.interfaces.sininterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpleadoCRUDArray {
 
+    List<Empleado> empleados = new ArrayList<>();
 
+    public void add(Empleado empleado) {
+        System.out.println("Agregando empleado...");
+        empleados.add(empleado);
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpleadoCRUDArray{" +
+                "empleados=" + empleados +
+                '}';
+    }
 }
