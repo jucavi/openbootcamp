16bbde59b20b8a155ae872f8b7fd9cd0afceecba
jucavi <jcarvil@gmail.com>
2022-08-30 10:05:33 +0200

    agregado empleado

--- Java-Basico/4.POO/ob_poo/src/poo/interfaces/sininterface/Empleado.java
+++ Java-Basico/4.POO/ob_poo/src/poo/interfaces/sininterface/Empleado.java
@@ -7,4 +7,22 @@ public class Empleado {
     int salario;
     boolean alta;
 
+    public Empleado() {}
+
+    public Empleado(String nombre, String apellido, int salario, boolean alta) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+        this.alta = alta;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", salario=" + salario +
+                ", alta=" + alta +
+                '}';
+    }
 }
