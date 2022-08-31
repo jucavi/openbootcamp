ff9792756a3da94067a4459c74393032c8f7d539
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-31 11:27:24 +0200

    agregado constructor y metodo toString clase coche ejercicio 5

--- Java-Basico/Ejercicios/ejercicio_5/src/com/openbootcamp/Coche.java
+++ Java-Basico/Ejercicios/ejercicio_5/src/com/openbootcamp/Coche.java
@@ -8,5 +8,20 @@ public class Coche {
 
     public Coche() {};
 
-    public Coche(String marca, String modelo, String color, int numPuertas) {};
+    public Coche(String marca, String modelo, String color, int numPuertas) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.numPuertas = numPuertas;
+    };
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", numPuertas=" + numPuertas +
+                '}';
+    }
 }
