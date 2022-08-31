d79483c3114aeeedfa6b4931034908cd00fc41e1
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-31 11:23:54 +0200

    agregado clase coche ejercicio 5

---
+++ Java-Basico/Ejercicios/ejercicio_5/src/com/openbootcamp/Coche.java
@@ -0,0 +1,12 @@
+package com.openbootcamp;
+
+public class Coche {
+    String marca;
+    String modelo;
+    String color;
+    int numPuertas;
+
+    public Coche() {};
+
+    public Coche(String marca, String modelo, String color, int numPuertas) {};
+}
