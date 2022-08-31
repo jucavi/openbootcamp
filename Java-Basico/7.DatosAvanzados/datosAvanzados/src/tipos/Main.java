b6dd39f1986a811fd7b903051a5694e82e3df1f5
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-08-31 16:35:02 +0200

    tipos de datos avanzados Main

--- Java-Basico/7.DatosAvanzados/datosAvanzados/src/tipos/Main.java
+++ Java-Basico/7.DatosAvanzados/datosAvanzados/src/tipos/Main.java
@@ -1,4 +1,13 @@
 package tipos;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Strings.demoStrings();
+        // Arrays.demoArrays();
+        // Vectores.demoVector();
+        // Listas.demoList();
+        Maps.demoMap();
+    }
 }
