4c25ad97b75932aade689d4cf4461e1ed7a7d7d6
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-09-13 10:13:23 +0200

    introducción Spring

---
+++ Spring/spring-helloworld/src/main/java/com/example/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class CalculatorService {
+
+    public CalculatorService() {
+        System.out.println("Ejecutando constructor CalculatorService");
+    }
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
+}
---
+++ Spring/spring-helloworld/src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        CalculatorService calculadora = (CalculatorService) context.getBean("calculatorService");
+
+        String texto = calculadora.holaMundo();
+        System.out.println(texto);
+    }
+}
