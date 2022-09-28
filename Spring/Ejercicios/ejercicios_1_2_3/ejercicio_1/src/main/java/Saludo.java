f827c03650126f4e06dc23fda5523ac48df15171
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-09-28 20:03:31 +0200

    Ejercicio 1 secciones 1, 2, 3

---
+++ Spring/Ejercicios/ejercicios_1_2_3/ejercicio_1/src/main/java/Saludo.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class Saludo {
+
+    public void imprimirSaludo() {
+        System.out.println("Hola desde el primer ejercicio planteado en la sección 3");
+    }
+}
