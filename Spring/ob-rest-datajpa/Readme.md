5ab879226fe6a008b7d6618c03c134fa3bf4187b
Juan Carlos Vilarrubia <jcarvil@gmail.com>
2022-09-29 21:17:48 +0200

    Spring sesion 7

--- Spring/ob-rest-datajpa/Readme.md
+++ Spring/ob-rest-datajpa/Readme.md
@@ -2,10 +2,13 @@
 ## Spring Boot
 
 Proyecto de Spring Boot con las dependencias / starters [Spring Boot](https://start.spring.io/)
-* H2
-* Spring Data JPA
-* Spring Web
-* Spring Boot Dev Tools
+* Starters para persistencia
+  * H2
+  * Spring Data JPA
+  
+* Starters para web
+  * Spring Web
+  * Spring Boot Dev Tools
 
 Aplicación API REST con acceso a base de datos H2 para persistir la información.
 
@@ -17,13 +20,13 @@ El acceso se puede realizar dede Postman o Navegador.
 
 ## Repository
 * BookRepository
-    * Interface extends Repository, JPARepository..
+    * Interface extends Repository, JPARepository ...
     * @Repository
 
 ## Controller
 * BookController
     * @RestController (RESTful), @Controller (mvc)
-    * @GetMapping, @PostMapping, @DeleteMapping
+    * @GetMapping, @PostMapping, @DeleteMapping ...
         1. Buscar todos los libros
         2. Buscar un libro
         3. Crear un nuevo libro
--- Spring/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controllers/BookController.java
+++ Spring/ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controllers/BookController.java
@@ -2,9 +2,13 @@ package com.example.obrestdatajpa.controllers;
 
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repositories.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,14 +17,16 @@ import java.util.Optional;
 public class BookController {
 
     private BookRepository bookRepository;
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
 
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     /**
+     * Devuelve todos los libros
      * https://localhost:8080/api/books
-     * @return
+     * @return ArrayList de libros
      */
     @GetMapping("/api/books")
     public List<Book> findAll() {
@@ -28,9 +34,10 @@ public class BookController {
     }
 
     /**
+     * Devuelve un libro localizado por su id
      * https://localhost:8080/api/books/id
-     * @param id
-     * @return
+     * @param id identificador unico de la base de datos
+     * @return ResponseEntity
      */
     @GetMapping("/api/books/{id}")
     public ResponseEntity<Book> findOneById(@PathVariable Long id) {
@@ -47,13 +54,64 @@ public class BookController {
     }
 
     /**
+     * Crea un libro
      * https://localhost:8080/api/books
-     * @param book
-     * @return
+     * @param book Libro a guardar
+     * @return ResponseEntity
      */
     @PostMapping("/api/books")
-    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers) {
+    public ResponseEntity<Book> create(@RequestBody Book book, @RequestHeader HttpHeaders headers) {
         System.out.println(headers.get("User-Agent"));
-       return bookRepository.save(book);
+
+        if (book.getId() != null) { // Ese libro ya existe
+            log.warn("trying to create an existing book with id " + book.getId());
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        Book res = bookRepository.save(book);
+        return ResponseEntity.ok(res);
+    }
+
+    /**
+     * Actualizar un libro existente
+     * https://localhost:8080/api/books
+     * @param book Libro modificado
+     * @return ResponseEntity
+     */
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> update(@RequestBody Book book) {
+        if (book.getId() == null) { // El libro no existe con lo cual no puede ser modificado
+            log.warn("trying to update a non existent book");
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (!bookRepository.existsById(book.getId())) {
+            log.warn("trying to update a non existent book id" + book.getId());
+
+            return ResponseEntity.notFound().build();
+        }
+
+        Book res = bookRepository.save(book);
+        return ResponseEntity.ok(res);
+    }
+
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Long id) {
+        try {
+            bookRepository.deleteById(id);
+        } catch (IllegalArgumentException e) {
+            log.warn("trying to delete a non existent book");
+
+            return ResponseEntity.badRequest().build();
+        } catch (EmptyResultDataAccessException e) {
+            log.warn("trying to delete a non existent book");
+
+            return ResponseEntity.notFound().build();
+        }
+
+
+        return ResponseEntity.noContent().build();
     }
 }
